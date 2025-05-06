65e1eebb41f00f374e1259b588153b3c4ff69833
kimia.jimenez <kimia.jimenez@visma.com>
2025-05-06 22:52:43 +0200

    Rename capa municipios

--- src/layers/municipios.model.js
+++ src/layers/municipios.model.js
@@ -1,11 +1,11 @@
 export const municipios = {
     "id": "municipios",
-    "title": "Municipios",
+    "title": "Delimitación de municipios",
     "type": "WMS",
     "url": "https://idena.navarra.es/ogc/wms",
     "layerNames": "IDENA:CATAST_Pol_Municipio",
-    "transparent": true,
-    "customParams": {
-        "CQL_FILTER": "CMUNICIPIO='201'"
-    }
+    "transparent": true
+    // "customParams": {
+    //     "CQL_FILTER": "CMUNICIPIO='201'"
+    // }
 }
